4ff8958d5cd95d04acebead3c1a1f4e8f1d68c26
yessine <yessine.amamou@gmail.com>
2026-04-04 12:44:26 +0100

    testing

--- ELTARTICLE.h
+++ ELTARTICLE.h
@@ -12,25 +12,22 @@ typedef struct {
 
 #define ELEMENT_VIDE NULL
 
-/* Crée un nouvel article */
+
 ELEMENT creerArticle(int id, const char *titre, const char *source, int score, int jour, int mois, int annee, int heure, int minute);
 
-/* Copie un article */
 ELEMENT copierArticle(ELEMENT art);
 
-/* Affiche un article */
 void afficherArticle(ELEMENT art);
 
-/* Affiche les infos courtes d'un article */
 void afficherArticleSimple(ELEMENT art);
 
-/* Libère la mémoire d'un article */
+
 void detruireArticle(ELEMENT art);
 
-/* Vérifie si deux articles sont identiques */
+
 int egauxArticles(ELEMENT art1, ELEMENT art2);
 
-/* Convertit un article en chaîne */
+
 char* articleToString(ELEMENT art);
 
 #endif
